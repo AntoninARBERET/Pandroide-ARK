60fb8f54f95a02e62403935b0627b213bf9846bd
AntoninARBERET <antonin.arberet@gmail.com>
2019-05-16 15:02:22 +0200

    Changed dockerfile to accept 0 to 4 cameras

--- docker/run_ark_image.sh
+++ docker/run_ark_image.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-#CAMS parameter must be 0 if you don't have 4 cameras connected
-CAMS=${1:-1}
-
+#CAMS must be the number of camera you want to use, default value is 4
+CAMS=${1:-4}
+echo "$CAMS"
 if [ $CAMS = 0 ]
 then
 	docker run -ti --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=all -e DISPLAY=$DISPLAY  -v /tmp/.X11-unix:/tmp/.X11-unix ark
+
+elif [ $CAMS = 1 ]
+then
+	docker run -ti --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=all -e DISPLAY=$DISPLAY --device=/dev/ttyUSB0 --device=/dev/video0:/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix ark
+
+elif [ $CAMS = 2 ]
+then
+	docker run -ti --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=all -e DISPLAY=$DISPLAY --device=/dev/ttyUSB0 --device=/dev/video0:/dev/video0 --device=/dev/video1:/dev/video1  -v /tmp/.X11-unix:/tmp/.X11-unix ark
+
+elif [ $CAMS = 3 ]
+then
+	docker run -ti --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=all -e DISPLAY=$DISPLAY --device=/dev/ttyUSB0 --device=/dev/video0:/dev/video0 --device=/dev/video1:/dev/video1 --device=/dev/video2:/dev/video2 -v /tmp/.X11-unix:/tmp/.X11-unix ark
+
+elif [ $CAMS = 4 ]
+then
+	docker run -ti --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=all -e DISPLAY=$DISPLAY --device=/dev/ttyUSB0 --device=/dev/video0:/dev/video0 --device=/dev/video1:/dev/video1 --device=/dev/video2:/dev/video2 --device=/dev/video3:/dev/video3 -v /tmp/.X11-unix:/tmp/.X11-unix ark
+
 else
-	docker run -ti --rm --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=all -e DISPLAY=$DISPLAY --device=/dev/video0:/dev/video0 --device=/dev/video1:/dev/video1 --device=/dev/video2:/dev/video2 --device=/dev/video3:/dev/video3 -v /tmp/.X11-unix:/tmp/.X11-unix ark
+	echo "Wrong number of cameras in parameters"
 fi
